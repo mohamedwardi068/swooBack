af8e162464cc0e50fbbf4f81f4878237f7713ac4
mohamedwardi068 <mohamedwardi068@gmail.com>
2025-12-11 21:59:12 +0100

    connect to atlas

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(
-    "mongodb://localhost:27017/swoo")
-
- //"mongodb+srv://mohamedwadi068:06112004@cluster0.gefeg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test"  ) 
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("connected to db");
   })
