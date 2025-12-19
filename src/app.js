392097779a0eebe8ecf8266f14235254ed167636
mohamedwardi068 <mohamedwardi068@gmail.com>
2025-12-19 12:23:12 +0100

    final

--- src/app.js
+++ src/app.js
@@ -5,10 +5,23 @@ const api = require("./routes/api");
 const app = express();
 
 // Middleware
+const allowedOrigins = [
+  "http://localhost:3000", // for local dev
+  "https://swoo.vercel.app/" // your Vercel frontend
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
-    credentials: true,
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true
   })
 );
 
